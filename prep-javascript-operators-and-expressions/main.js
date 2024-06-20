def245c3cb1192e9eef18a7f326793df6c7f3913
ajowyang <andrewowyang875@gmail.com>
2024-06-20 08:22:56 +0000

    Completed pre-javascript-operators-and-expressions exercise -Andrew Owyang

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+let number1 = 3;
+let number2 = 7;
+let product = number1 * number2;
+console.log('Value of product:', product);
+console.log('Type of produce:', typeof product);
+
+let charge = 80;
+let payment = 51;
+let amountRemaining = charge - payment;
+console.log('Value of amountRemaining:', amountRemaining);
+console.log('Type of amountRemaining:', typeof amountRemaining);
+
+let tests = 39;
+let assignments = 71;
+let final = 99;
+let grade = (tests + assignments + final) / 3;
+console.log('Value of grade:', grade);
+console.log('Type of grade:', typeof grade);
+
+let firstName = 'Andrew';
+let lastName = 'Owyang';
+let fullName = firstName + ' ' + lastName;
+console.log('Value of full Name:', fullName);
+console.log('Type of fullName:', typeof fullName);
+
+let pH = 3;
+let isAcidic = pH < 7;
+console.log('Value of isAcidic:', isAcidic);
+console.log('Type of isAcidic', typeof isAcidic);
+
+let numberOfSoldiers = 21;
+let isSparta = numberOfSoldiers === 300;
+console.log('Value of isSparta:', isSparta);
+console.log('Type of isSparta:', typeof isSparta);
+
+let nickname = fullName;
+nickName = nickname + '  is the GOAT';
+console.log('Value of nickname:', nickname);
+console.log('Type of nickname:', typeof nickname);
